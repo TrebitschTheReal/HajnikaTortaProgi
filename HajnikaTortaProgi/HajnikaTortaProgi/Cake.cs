cb56fdca11af6f91a69c92847ca2cc4fcf49be4f
Viktor Dohányos <vorkapp@gmail.com>
2019-05-12 21:00:00 +0200

    1

---
+++ HajnikaTortaProgi/HajnikaTortaProgi/Cake.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajnikaTortaProgi
+{
+    class Cake
+    {
+        //mililiterben
+        private int tojasMennyiseg;
+        private int tojasEgysegAr;
+
+
+
+        private int liszt;
+        private int tej;
+        private int cukor;
+        private int olaj;
+        private int tejfol;
+        private int tejszin;
+        private int mascarpone;
+        private int kakao;
+        private int sutoPor;
+        private int pudingPor;
+        private int vaj;
+        private int vanillia;
+        private int lime;
+        private int citrom;
+        private int etcsoki;
+        private int tejcsoki;
+        private int fehercsoki;
+        private int zselatin;
+        private int mokusMogyoro;
+        private int foldiMogyoro;
+        private int meggy;
+        private int meggyLe;
+        private int osziBarack;
+        private int nutella;
+        private int gesztenyeMassza;
+        private int narancs;
+        private int mak;
+        private int oreo;
+        private int joghurt;
+    }
+}
+
+/* 
+ 
+Egy alapanyag class ami rendelkezik 4 adattaggal: név, mennyiség, egységár, egységtípus
+
+az alapanyagokat egy txt-ben tárolni amit a program induláskor beolvas és aszerint tölt fel egy alapanyag tömböt egy alapanyag példányból (mint az employee cuccnál)
+
+
+*/
---
+++ HajnikaTortaProgi/HajnikaTortaProgi/Export.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HajnikaTortaProgi
+{
+    class Export
+    {
+        private static string accesPath = "E:\\HajnikaTortaProgi\\alapanyagok.csv";
+
+        public void ExportOutToTxt(Cake)
+        {
+            StreamWriter sw = new StreamWriter(accesPath);
+
+            for (int i = 0; i < Cake.Employees.GetLength(0); i++)
+            {
+                sw.WriteLine(Cake.Employees[i].Name);
+                sw.WriteLine(Cake.Employees[i].Position);
+                sw.WriteLine(Cake.Employees[i].WageInString + " Ft.");
+                sw.WriteLine("Productivity: " + Cake.Employees[i].Productivity + " %");
+                sw.WriteLine(Cake.Employees[i].MonitorType);
+                sw.WriteLine(Cake.Employees[i].ProcessorType);
+                sw.WriteLine(Cake.Employees[i].ProcessorPerformance + "Mhz.");
+                sw.WriteLine("==============================");
+            }
+            sw.Close();
+            Console.WriteLine("Exporting done to {0}", accesPath);
+        }
+    }
+} 
+
+}
